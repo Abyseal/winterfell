bd76ce70e37e8ba68f0cf84151583ae18bd4576e
Abyseal <115791417+Abyseal@users.noreply.github.com>
2025-02-07 18:17:17 +0300

    Add files via upload

---
+++ include/winterfell/winterfell.h
@@ -0,0 +1,8 @@
+#ifndef WINTERFELL
+#define WINTERFELL
+
+#include<spdlog/spdlog.h>
+
+void winterfell_motto();
+
+#endif // !DEBUG
---
+++ src/winterfell.cpp
@@ -0,0 +1,6 @@
+#include<winterfell/winterfell.h>
+#include<iostream>
+
+void winterfell_motto(){
+  std::cout<<"Winter is close"<<std::endl;
+}
